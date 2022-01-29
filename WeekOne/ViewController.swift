fd17817f23e3021072d9248ff1ebd9a6970b40fc
BathingCorgi <daniel.sh.hong@gmail.com>
2022-01-29 16:36:24 -0500

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by  Daniel Hong on 1/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
